90ffc353e6f6d624658828be00d9cbfa4e5d0793
PhotonNikko <69907559+photon-niko@users.noreply.github.com>
2022-05-13 15:32:08 +0700

    no debug parser ig

--- circles/main.py
+++ circles/main.py
@@ -9,14 +9,7 @@ def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument("path", type=str, help="Path to the file to interpret")
     argparser.add_argument("-v", "--vision", action="store_true", help="Show what parser sees")
-    argparser.add_argument("-d", "--debug", action="store_true", help="Ignore path and run DebugParser")
     args = argparser.parse_args()
-
-    if args.debug:
-        parser = DebugParser(6)
-        parser.parse()
-        parser.loop()
-        return
 
     assert Path(args.path).is_file(), "File does not exist"
     
